74387afe180749e090cca9ad44193d5674950e8e
Victor Hugo <144183601+VictorHugo71@users.noreply.github.com>
2024-02-13 20:35:16 -0300

    Iniciando estudos em estruturas de condições

---
+++ aula004/ex008.js
@@ -0,0 +1,6 @@
+var vel = 122
+console.log(`A velocidade do seu carro é de: ${vel}km/h`)
+if (vel > 60) { //Concição Simples
+    console.log('Você ultrapassou a velocidade limite. MULTADO!!')
+}
+console.log('Dirija sempre usando o cinto de segurança')

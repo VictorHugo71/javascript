e12d49137229180062798db42f5352d95fc988e4
Victor Hugo <144183601+VictorHugo71@users.noreply.github.com>
2024-03-10 19:40:02 -0300

    aprendendo a usar do, while e for no JS

---
+++ ambiente/njavas.js
@@ -0,0 +1,12 @@
+/*var c = 1
+while (c<=10) {
+    console.log(`olá ${c}`)
+    c++
+}*/
+
+//--------------------
+console.log('Vai Começar')
+for (var c=1; c<=5; c++) {
+    console.log(`Olá ${c}`)
+}
+console.log('Fim')
---
+++ ambiente/script.js
@@ -0,0 +1,17 @@
+var cont = 1000
+do {
+    console.log(`Olá esta é a ${cont}° vez`)
+    cont++
+} while (cont <= 100)
+
+/*while (cont < 100) {
+    console.log(`Olá esta é a ${cont}° vez`)
+    cont++
+}*/
+
+/*console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')
+console.log('Tudo Bem!')*/

badeb37f75f4b29453877ec3f1fc97973cbbd087
Victor Hugo <144183601+VictorHugo71@users.noreply.github.com>
2024-03-10 21:09:00 -0300

    Inicio dos exercicios de repetição

---
+++ aula007/ex003/script.js
@@ -0,0 +1,11 @@
+function contar() {
+    var n1 = document.getElementById('n1')
+    var n2 = document.getElementById('n2')
+    var passo = document.getElementById('ipasso')
+    num1 = Number(n1.value) 
+    num2 = Number(n2.value)
+    pas = Number(passo.value)
+
+    var res = document.querySelector('div#res')
+    
+}

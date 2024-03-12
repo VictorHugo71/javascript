f8dcf3e44676f76ac51b2fa133999f4c7f53c1e4
Victor Hugo <144183601+VictorHugo71@users.noreply.github.com>
2024-03-12 19:28:40 -0300

    Finalização do exercicio do super contador

--- aula007/ex003/script.js
+++ aula007/ex003/script.js
@@ -1,11 +1,30 @@
 function contar() {
-    var n1 = document.getElementById('n1')
-    var n2 = document.getElementById('n2')
-    var passo = document.getElementById('ipasso')
-    num1 = Number(n1.value) 
-    num2 = Number(n2.value)
-    pas = Number(passo.value)
+    let ini = document.getElementById('n1')
+    let fim = document.getElementById('n2')
+    let passo = document.getElementById('ipasso')
+    let res = document.querySelector('div#res')
 
-    var res = document.querySelector('div#res')
-    
+    if (n1.value.length == 0 || n2.value.length == 0 || passo.value.length == 0) {
+        window.alert("Preencha os dados requisitados corretamente e tente novamente")
+        res.innerHTML = "Impossível Contar"
+    } else {
+        res.innerHTML = "Contando:"
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo Inválido')
+            p = 1
+        }
+        if (i < f) {
+            for(let c = i; c <= f; c += p) { //Contagem Crescente
+                res.innerHTML += `${c}\u{1F449} `
+            }
+        } else { //Contagem Regressiva
+            for(let c = i; c >= f; c-= p) {
+                res.innerHTML += `${c}\u{1F449} `
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
+    }
 }
